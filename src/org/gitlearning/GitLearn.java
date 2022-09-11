830193829dc1169f47687a5e0b0f57e07b44daa2
vj2597m <vj052597@gmail.com>
2022-09-11 22:49:20 +0530

    vijay888

---
+++ src/org/gitlearning/GitLearn.java
@@ -0,0 +1,20 @@
+package org.gitlearning;
+
+public class GitLearn {
+
+	private void employee() {
+		System.out.println("riya");
+
+	}
+	private void employeeId() {
+		System.out.println("45685");
+
+	}
+
+	public static void main(String[] args) {
+
+		GitLearn g=new GitLearn();
+		g.employee();
+		g.employeeId();
+	}
+}
